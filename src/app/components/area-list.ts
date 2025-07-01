de263330ef821167414521e0c204477b5fd934b9
Fatima Bdair <fatimabdair@fatimas-air.home>
2025-06-30 20:28:38 -0400

    assignment1

---
+++ src/app/components/area-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-list',
+  imports: [],
+  templateUrl: './area-list.html',
+  styleUrl: './area-list.scss'
+})
+export class AreaList {
+
+}
---
+++ src/app/model/reservation.model.ts
@@ -0,0 +1,8 @@
+export interface Reservation{
+    id?: number;
+    areaId: number;
+    name: string;
+    email: string;
+    date: string;
+    time: string;
+}
